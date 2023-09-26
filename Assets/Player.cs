0fc85d85b40f46225a0bb76b8f001e6012ed4b82
Project-Soar <109503862+Project-Soar@users.noreply.github.com>
2023-09-26 15:39:40 +0530

    Turn undead log 1
    player controller created

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,26 +4,153 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public PlayerStateMachine stateMachine{get; private set;}
+    private Rigidbody2D rb;
 
-    public PlayerIdleState idleState{get; private set;}
-    public PlayerMoveState moveState{get; private set;}
+    private Animator anim;
 
-    private void Awake()
+    [Header("Move Info")]
+    public float moveSpeed;
+    private bool canDoubleJump;
+    private float movingInput;
+    public float jumpForce;
+    private bool canMove;
+    public Vector2 wallJumpDirection;
+
+    [Header("Collision Info")]
+    public LayerMask whatIsGround;
+    public float groundCheckDistance;
+    public float wallCheckDistance;
+    private bool isGrounded;
+    private bool isWallDetected;
+    private bool canWallSlide;
+    private bool isWallSliding;
+
+
+    private bool facingRight = true;
+    private int facingDirection = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-        stateMachine = new PlayerStateMachine();
-
-        idleState = new PlayerIdleState(this,stateMachine, "Idle");
-        moveState = new PlayerMoveState(this, stateMachine , "Move");
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    private void Start()
+    // Update is called once per frame
+    void Update()
     {
-        stateMachine.Initialize(idleState);
+        AnimationController();
+
+        CollisionCheck();
+
+        InputChecks();
+        
+        FlipController();
+
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+            canMove = true;
+        }
+        if(canWallSlide)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        
+        Move();
     }
 
-    private void Update()
+    private void Move()
     {
-        stateMachine.currentState.Update();
+        if(canMove)
+            rb.velocity = new Vector2(moveSpeed * movingInput , rb.velocity.y);
+    }
+
+    private void AnimationController()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isWallSliding", isWallSliding);
+        anim.SetBool("isWallDetected",isWallDetected);
+    }
+
+    private void InputChecks()
+    {
+        movingInput = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpButton();
+                
+        }
+        if(Input.GetAxis("Vertical") < 0)
+            canWallSlide = false;
+    }
+    private void JumpButton()
+    {
+        if(isWallSliding)
+        {
+            WallJump();
+        }
+        else if(isGrounded)
+        {
+            Jump();
+        }else if(canDoubleJump)
+        {
+            canDoubleJump =false;
+            Jump();
+        }
+
+        canWallSlide = false;
+    }
+
+    private void WallJump()
+    {
+        canMove = false;
+        rb.velocity = new Vector2(wallJumpDirection.x* - facingDirection, wallJumpDirection.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void FlipController()
+    {
+        if(facingRight && rb.velocity.x<0)
+            Flip();
+        else if(!facingRight && rb.velocity.x>0)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingDirection = facingDirection*-1;
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+    }
+
+    private void CollisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDirection, wallCheckDistance , whatIsGround);
+        
+        if(isWallDetected && rb.velocity.y <0)
+            canWallSlide = true;
+        if(!isWallDetected)
+        {
+            isWallSliding = false;
+            canWallSlide = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + wallCheckDistance * facingDirection, transform.position.y));
     }
 }
